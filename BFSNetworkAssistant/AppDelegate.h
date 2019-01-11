9e1dc9e08d3ee06d8811a022da10afde52b0d49b
Alex BF <alexbf@AlexdeMac.local>
2019-01-11 23:50:13 +0800

    版本开始

---
+++ BFSNetworkAssistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFSNetworkAssistant
+//
+//  Created by Alex BF on 2019/1/11.
+//  Copyright © 2019年 Alex BF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFSNetworkAssistant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFSNetworkAssistant
+//
+//  Created by Alex BF on 2019/1/11.
+//  Copyright © 2019年 Alex BF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
